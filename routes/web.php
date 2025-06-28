595138474feb9b5691a08d84379019c81040f315
rhyru9 <reyhansyah4@gmail.com>
2025-06-28 21:53:42 +0700

    finishh

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,6 @@ use App\Http\Controllers\Dosen\{
     DosenBimbinganController,
     KomentarBimbinganController,
 };
-
 use App\Http\Controllers\mhs\{
     DashboardMhsController,
     MhsBimbinganController,
@@ -53,9 +52,9 @@ Route::middleware('auth')->group(function () {
 
 /*
 |--------------------------------------------------------------------------
-| RUTE ADMIN - TAMBAHAN UNTUK EDIT METHODS
+| RUTE ADMIN
 |--------------------------------------------------------------------------
-| Tambahkan route ini ke dalam middleware group admin yang sudah ada
+| Rute khusus untuk pengguna dengan role admin (role:1)
 */
 Route::middleware(['auth', 'role:1'])->prefix('admin')->group(function () {
     // Dashboard Admin
@@ -68,40 +67,39 @@ Route::middleware(['auth', 'role:1'])->prefix('admin')->group(function () {
     // Manajemen Pengguna
     Route::get('/users', [UserController::class, 'index'])->name('admin.users.index');
 
-    // Create routes
+    // Create Routes
     Route::get('/users/create', [UserController::class, 'create'])->name('admin.users.create');
     Route::get('/users/create/admin', [UserController::class, 'createAdmin'])->name('admin.users.create.admin');
     Route::get('/users/create/dosen', [UserController::class, 'createDosen'])->name('admin.users.create.dosen');
     Route::get('/users/create/mahasiswa', [UserController::class, 'createMahasiswa'])->name('admin.users.create.mahasiswa');
 
-    // Store routes
+    // Store Routes
     Route::post('/users/store', [UserController::class, 'store'])->name('admin.store');
     Route::post('/users/store/admin', [UserController::class, 'storeAdmin'])->name('admin.store.admin');
     Route::post('/users/store/dosen', [UserController::class, 'storeDosen'])->name('admin.store.dosen');
     Route::post('/users/store/mahasiswa', [UserController::class, 'storeMahasiswa'])->name('admin.store.mahasiswa');
 
-    // Edit routes - TAMBAHAN BARU
+    // Edit Routes
     Route::get('/users/{user}/edit/admin', [UserController::class, 'editAdmin'])->name('admin.edit.admin');
     Route::get('/users/{user}/edit/dosen', [UserController::class, 'editDosen'])->name('admin.edit.dosen');
     Route::get('/users/{user}/edit/mahasiswa', [UserController::class, 'editMahasiswa'])->name('admin.edit.mahasiswa');
 
-    // Update routes - TAMBAHAN BARU
+    // Update Routes
     Route::put('/users/{user}/update/admin', [UserController::class, 'updateAdmin'])->name('admin.update.admin');
     Route::put('/users/{user}/update/dosen', [UserController::class, 'updateDosen'])->name('admin.update.dosen');
     Route::put('/users/{user}/update/mahasiswa', [UserController::class, 'updateMahasiswa'])->name('admin.update.mahasiswa');
 
-    // Show, Edit, Update, Delete (existing routes)
+    // Standard CRUD Routes
     Route::get('/users/{user}', [UserController::class, 'show'])->name('admin.show');
     Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('admin.edit');
     Route::put('/users/{user}', [UserController::class, 'update'])->name('admin.update');
     Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('admin.destroy');
 
-    // Manajemen Resource (CRUD Lengkap)
+    // Resource Routes (CRUD Lengkap)
     Route::resource('tahun_ajar', TahunAjarController::class);
     Route::resource('program_studi', ProgramStudiController::class);
     Route::resource('bimbingan', BimbinganController::class);
 });
-
 
 /*
 |--------------------------------------------------------------------------
@@ -112,6 +110,7 @@ Route::middleware(['auth', 'role:1'])->prefix('admin')->group(function () {
 Route::middleware(['auth', 'role:2'])->prefix('dosen')->group(function () {
     // Dashboard Dosen
     Route::get('/dashboard', [DashboardDosenController::class, 'index'])->name('dosen.dashboard');
+
     Route::prefix('dashboard')->group(function () {
         Route::get('/check-new-bimbingan', [DashboardDosenController::class, 'checkNewBimbingan'])->name('dosen.check-new-bimbingan');
         Route::post('/tandai-dibaca/{id}', [DashboardDosenController::class, 'tandaiSudahDibaca'])->name('dosen.mark-as-read');
@@ -119,7 +118,8 @@ Route::middleware(['auth', 'role:2'])->prefix('dosen')->group(function () {
         Route::get('/statistik', [DashboardDosenController::class, 'getStatistik'])->name('dosen.statistik');
         Route::get('/bimbingan/{status}', [DashboardDosenController::class, 'getBimbinganByStatus'])->name('dosen.bimbingan-by-status');
     });
-    //profile
+
+    // Manajemen Profil Dosen
     Route::get('/profil', [DosenProfileController::class, 'edit'])->name('dosen.profile.edit');
     Route::post('/profil', [DosenProfileController::class, 'update'])->name('dosen.profile.update');
 
@@ -146,7 +146,8 @@ Route::middleware(['auth', 'role:2'])->prefix('dosen')->group(function () {
 Route::middleware(['auth', 'role:3'])->prefix('mhs')->group(function () {
     // Dashboard Mahasiswa
     Route::get('/dashboard', [DashboardMhsController::class, 'index'])->name('mahasiswa.dashboard');
-    // Manajemen Profil mahaiswa
+
+    // Manajemen Profil Mahasiswa
     Route::get('/profil', [MhsProfileController::class, 'edit'])->name('mhs.profile.edit');
     Route::post('/profil', [MhsProfileController::class, 'update'])->name('mhs.profile.update');
 
@@ -158,8 +159,7 @@ Route::middleware(['auth', 'role:3'])->prefix('mhs')->group(function () {
     Route::get('/bimbingan/{id}/edit', [MhsBimbinganController::class, 'edit'])->name('mhs.bimbingan.edit');
     Route::put('/bimbingan/{id}', [MhsBimbinganController::class, 'update'])->name('mhs.bimbingan.update');
     Route::delete('/bimbingan/{id}', [MhsBimbinganController::class, 'destroy'])->name('mhs.bimbingan.destroy');
-        // Manajemen Komentar Bimbingan
+
+    // Manajemen Komentar Bimbingan
     Route::post('/bimbingan/{id_bimbingan}/komentar', [KomentarMahasiswaController::class, 'store'])->name('bim.komentar.store');
 });
-
-
